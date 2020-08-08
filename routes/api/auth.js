9687f09af44d5e443f43ac2671ef5947ebe3abc9
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-08-08 22:37:54 +0000

    adding authenfication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,17 @@
 const  express = require('express')
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const {check, validationResult} = require('express-validator');
+
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User')
+
 /*
 *@Route GET api/auth
-*@description test auth route
+*@description auth route
 *@access public
 */
 
@@ -17,6 +23,54 @@ router.get('/', auth, async (req, res)=>{
         console.error(error.message);
         res.status(500).send('Erreur Interne')
     }
+})
+
+/*
+*@Route POST api/auth
+*@description Authenticate User and get token
+*@access public
+*/
+router.post('/',
+    [
+        check('email', 'Entrer une adresse email valide').isEmail(),
+        check('password', 'le mot de passe obligatoire').exists()
+    ],
+    async (req, res)=>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        const {name, email, password} = req.body;
+        
+        try {
+            //check if user exists
+            let user = await User.findOne({email});
+            if(!user){
+                res.status(400).json({errors:[{msg:"email ou mot de passe invalide"}]})
+            }
+        const isMatch =  await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({errors:[{msg:'email ou mot de passe invalide'}]})
+        }
+        //return json webtoken
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload,
+             config.get("jwtSecret"),
+             {expiresIn:360000}, 
+             (error, token)=>{
+                 if(error) throw error;
+                 res.json({token})
+             })
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Erreur Interne')
+        }
+        
 })
 
 
